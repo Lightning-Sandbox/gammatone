3077a9039879566961f7951f92035daf15a3db8f
Jirka <jirka.borovec@seznam.cz>
2023-06-29 13:49:34 +0200

    np 1.13

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages = find_packages(),
 
     install_requires = [
-        'numpy <1.17.0',  # todo: search for actually compatible version
+        'numpy <1.13.0',  # todo: search for actually compatible version
         'scipy',
         'nose',
         'mock',
