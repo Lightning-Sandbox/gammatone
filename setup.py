7f6f21eaa563f6c94494216975f0379e7b910042
Jirka <jirka.borovec@seznam.cz>
2024-06-06 00:49:45 +0200

    ci: use `pytest` & bump python

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests"]),
     install_requires=["numpy", "scipy", "matplotlib"],
-    extras_require={"test": ["nose", "mock"]},
+    extras_require={"test": ["pytest", "mock"]},
     entry_points={"console_scripts": ["gammatone = gammatone.plot:main"]},
     classifiers=[
         "Environment :: Console",
